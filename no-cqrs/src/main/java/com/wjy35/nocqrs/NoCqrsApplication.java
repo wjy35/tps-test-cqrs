8e9efc62df5b31ff0506469fb9b756a6b2445b4a
왕준영 <wangsun7@naver.com>
2023-12-17 06:23:11 +0900

    :heavy_plus_sign: Feat: init Project

---
+++ no-cqrs/src/main/java/com/wjy35/nocqrs/NoCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.wjy35.nocqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoCqrsApplication.class, args);
+    }
+
+}
---
+++ no-cqrs/src/test/java/com/wjy35/nocqrs/NoCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjy35.nocqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
