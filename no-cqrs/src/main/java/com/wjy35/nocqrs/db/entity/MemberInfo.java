6121a800932e2c6da6396ce18cd5caf4a8b67885
왕준영 <wangsun7@naver.com>
2023-12-18 06:25:27 +0900

    :hammer: Refactor: MemberInfo 생성자 수정

--- no-cqrs/src/main/java/com/wjy35/nocqrs/db/entity/MemberInfo.java
+++ no-cqrs/src/main/java/com/wjy35/nocqrs/db/entity/MemberInfo.java
@@ -3,15 +3,14 @@ package com.wjy35.nocqrs.db.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Table(name = "member_info")
 @Entity
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class MemberInfo {
     @Id
     private Long memberId;
@@ -19,4 +18,8 @@ public class MemberInfo {
     private String nickname;
 
     private String name;
+
+    public void updateNickname(String nickname){
+        this.nickname = nickname;
+    }
 }
